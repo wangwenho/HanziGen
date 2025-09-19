e3dac1f1789b47d8d64bf517f0020d820499c08e
wangwenho <wangwenho@gmail.com>
2025-09-19 09:24:35 +0800

    feat: update output directory structure for metric computation and SVG conversion

--- scripts/convert_to_svg.sh
+++ scripts/convert_to_svg.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 TARGET_FONT_PATH="fonts/851Gkktt_005.ttf"
+TIMESTAMP="20250918_215849"
 BLACKLEVEL=0.5
 TURDSIZE=2
 ALPHAMAX=1.0
@@ -9,8 +10,8 @@ OPTTOLERANCE=0.2
 
 TARGET_FONT_NAME="$(basename "$TARGET_FONT_PATH" | sed -E 's/\.(ttf|otf)$//')"
 
-INPUT_DIR="samples_${TARGET_FONT_NAME}/inference/gen"
-OUTPUT_DIR="svgs_${TARGET_FONT_NAME}"
+INPUT_DIR="samples_${TARGET_FONT_NAME}_inference_${TIMESTAMP}/inference/gen/"
+OUTPUT_DIR="svgs_${TARGET_FONT_NAME}_${TIMESTAMP}/"
 
 python convert_to_svg.py \
     --input_dir "$INPUT_DIR" \
